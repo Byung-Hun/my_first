b30f063653e58339202c013d8d4eabc8226f2eb4
byunghun <rheebh1@naver.com>
2020-10-22 15:26:49 +0900

    main 수정

--- pages/index.vue
+++ pages/index.vue
@@ -6,22 +6,7 @@
         first_my_project
       </h1>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      
       </div>
     </div>
   </div>
